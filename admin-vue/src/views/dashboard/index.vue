652685c69ca019fd65b264e2d095d7d826daefa9
rwchen <rw-chen@foxmail.com>
2024-04-19 20:21:36 +0800

    vue

---
+++ admin-vue/src/views/dashboard/index.vue
@@ -0,0 +1,49 @@
+  <template>
+  <div class="dashboard-container">
+    <div class="dashboard-text">
+      欢迎登录 湖北工业大学工程技术学院 校园博客管理系统
+    </div>
+    <div class="content">
+      
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+export default {
+  name: 'Dashboard',
+  computed: {
+    ...mapGetters([
+      'name'
+    ])
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.dashboard {
+  &-container {
+    margin: 20px;
+  }
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+    text-align: center;
+    font-family:'楷体';
+    color: darkcyan;
+  }
+}
+
+.content {
+  margin-top: 50px;
+  margin-left: 150px;
+  // background-color: #c23e3e; /* 设置背景颜色为浅灰色 */  
+  background-image: url("../../../img/school_log.png");
+  // url("http://storage.rwchen.top/school_log.png");
+  background-size: cover; /* 覆盖整个容器 */  
+  background-position: center; /* 图片居中显示 */  
+  width: 1500px;  
+  height: 640px;
+}
+</style>
---
+++ blog-vue/src/api/comment.js
@@ -0,0 +1,38 @@
+﻿import request from '@/utils/request'
+
+// 发送文章评论
+export function sendComment(type,articleId,rootId,toCommentId,toCommentUserId,content) {
+    return request({
+        url: '/comment',
+        method: 'post',
+        data: {"articleId":articleId,"type":type,"rootId":rootId,"toCommentId":toCommentId,"toCommentUserId":toCommentUserId,"content":content}
+    })
+}
+
+// 获取文章是否可评论
+export function canComment(articleId) {
+    return request({
+        url: '/comment/canComment/' + articleId,
+        method: 'get',
+    })
+}
+
+export function getArticleComment(query) {
+    return request({
+        url: '/comment/commentList',
+        method: 'get',
+        headers: {
+          isToken: false
+        },
+        params: query
+    })
+}
+
+
+export function getLinkComment(query) {
+    return request({
+        url: '/comment/linkCommentList',
+        method: 'get',
+        params: query
+    })
+}
